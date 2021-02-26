baec6857f5a842d47a535893f81d71ea2b5079c5
Luis Hernandez <lhdez@iuma.ulpgc.es>
2021-02-26 18:04:59 +0000

    cambios menores

--- app/src/androidTest/java/es/ulpgc/eite/da/basicquizlab/test/QuizSteps.java
+++ app/src/androidTest/java/es/ulpgc/eite/da/basicquizlab/test/QuizSteps.java
@@ -59,7 +59,7 @@ public class QuizSteps {
   @And("^ocultar resultado$")
   public void ocultarResultado() {
     //onView(withId(R.id.replyText)).check(matches(not(isDisplayed())));
-    onView(withId(R.id.replyText)).check(matches(isDisplayed()));
+    //onView(withId(R.id.replyText)).check(matches(isDisplayed()));
     //onView(withId(R.id.replyText)).check(matches(withText("???")));
     onView(withId(R.id.replyText))
         .check(matches(withText(activity.getString(R.string.empty_text))));
@@ -91,7 +91,7 @@ public class QuizSteps {
 
   @And("^ocultar respuesta$")
   public void ocultarRespuesta() {
-    onView(withId(R.id.answerText)).check(matches(isDisplayed()));
+    //onView(withId(R.id.answerText)).check(matches(isDisplayed()));
     //onView(withId(R.id.answerText)).check(matches(withText("???")));
     onView(withId(R.id.answerText))
         .check(matches(withText(activity.getString(R.string.empty_text))));
